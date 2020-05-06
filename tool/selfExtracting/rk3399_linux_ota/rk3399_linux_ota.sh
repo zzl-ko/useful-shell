57f9a295d14dbf24a21f458ac4627df844f01e0d
Kevin Zhou <kevin.zhou@robotemi.com>
2020-05-06 14:52:58 +0800

    优化rk3399 linux OTA脚本,支持组件的选择性打包

--- tool/selfExtracting/rk3399_linux_ota/rk3399_linux_ota.sh
+++ tool/selfExtracting/rk3399_linux_ota/rk3399_linux_ota.sh
@@ -8,13 +8,16 @@
 # History : Please check the end of file.
 ###############################################################################
 
+BAK_BLK_CNT=2
+BAK_BLK_SKP=1
 SOM_IMG_VER=0
 SOM_IMG_NAM=ota.img
 SOM_IMG_MD5=0
+TYPE_OF_IMG=none
 KERNEAL_VER="$(uname -v)"
+
 VERSION_FIL=~/Desktop/Image_Version
 TEMI_PASSWD='Algo!@Robo#$01%10^18'
-
 SCRIPT_PATH=`dirname "$(realpath $0)"`
 SCRIPT_NAME=`basename "$(realpath $0)"`
 EXEREALPATH=$(realpath $0)
@@ -24,6 +27,7 @@ OTA_TMP_DIR='.rk3399_linux_ota'
 TMPFILEPATH=/tmp/readout.img
 IMG_DEV_NAM=/dev/mmcblk1
 IMG_BLK_CNT=180223
+IMG_BLK_SKP=1
 
 function ota_info()
 {
@@ -59,41 +63,103 @@ function ota_info()
 function ota_usages()
 {
     echo 
-    echo "X3388 Linux OTA Usage:"
-    echo "./ota.run [-d] [-u [-v version]] [-n [-v version]] [-h]"
+    echo "X3399 Linux OTA Version $SOM_IMG_VER Usage:"
+    echo "./ota.run [-d] [-N [-v version]] [-n [-v version] [-lLuUtTmMrRkKB] [-h]"
     echo "-d deploy the ota package."
-    echo "-u update the ota package."
     echo "-n create a new ota package."
+    echo "-N update the ota package."
     echo "-v the version of ota package."
+    echo "-l read from loader to boot"
+    echo "-L only read loader"
+    echo "-u read from uboot to boot"
+    echo "-U only read uboot"
+    echo "-t read from trust to boot"
+    echo "-T only read trust"
+    echo "-m read from misc to boot"
+    echo "-M only read misc"
+    echo "-r read from resource(dtb) to boot"
+    echo "-R only read resource(dtb)"
+    echo "-k read from kernel to boot"
+    echo "-K only read kernel"
+    echo "-B only read boot"
     echo "-h display the help info."
     echo "for example:"
-    echo "create a execable ota: ./ota -n -v 1.9"
+    echo "create a execable ota: ./ota -n -v 1.9 [-k]"
     echo "deploy a execable ota: ./ota [-d]"
+    echo 
+    echo "TYPE_OF_IMG: $TYPE_OF_IMG"
+    echo "BAK_BLK_CNT: $BAK_BLK_CNT"
+    echo "BAK_BLK_SKP: $BAK_BLK_SKP"
+    echo "IMG_BLK_CNT: $IMG_BLK_CNT"
+    echo "IMG_BLK_SKP: $IMG_BLK_SKP"
+    echo "KERNEAL_VER: $KERNEAL_VER"
+}
+
+function ota_cmdline_parser()
+{
+    local flag=0
+
+#("0x00002000+0x00002000+0x00002000+0x00002000+0x00008000+0x0000C000+0x00010000 - 1", opensource.rock-chips.com/wiki_Partitions)
+#( reserved   uboot      trust      misc       resource   kernel     boot        MBR, refer parameter.txt)
+
+    while getopts ":hdnNv:lLuUtTmMrRkKB" opt > /dev/null; do
+        case "$opt" in
+            d) flag=1 ;;
+            n) flag=2 ;;
+            N) flag=3 ;;
+            v) SOM_IMG_VER="$OPTARG" ;;
+            l) IMG_BLK_SKP=64     ; IMG_BLK_CNT=7104   ; TYPE_OF_IMG="-l (from loader)"   ;;
+            L) IMG_BLK_SKP=64     ; IMG_BLK_CNT=8191   ; TYPE_OF_IMG="-L (only loader)"   ;;
+            u) IMG_BLK_SKP=8192   ; IMG_BLK_CNT=172032 ; TYPE_OF_IMG="-u (from uboot)"    ;;
+            U) IMG_BLK_SKP=8192   ; IMG_BLK_CNT=8192   ; TYPE_OF_IMG="-U (only uboot)"    ;;
+            t) IMG_BLK_SKP=16384  ; IMG_BLK_CNT=163840 ; TYPE_OF_IMG="-t (from trust)"    ;;
+            T) IMG_BLK_SKP=16384  ; IMG_BLK_CNT=8192   ; TYPE_OF_IMG="-T (only trust)"    ;;
+            m) IMG_BLK_SKP=24576  ; IMG_BLK_CNT=155648 ; TYPE_OF_IMG="-m (from misc)"     ;;
+            M) IMG_BLK_SKP=24576  ; IMG_BLK_CNT=8192   ; TYPE_OF_IMG="-M (only misc)"     ;;
+            r) IMG_BLK_SKP=32768  ; IMG_BLK_CNT=147456 ; TYPE_OF_IMG="-r (from resource)" ;;
+            R) IMG_BLK_SKP=32768  ; IMG_BLK_CNT=32768  ; TYPE_OF_IMG="-R (only resource)" ;;
+            k) IMG_BLK_SKP=65536  ; IMG_BLK_CNT=114688 ; TYPE_OF_IMG="-k (from kernel)"   ;;
+            K) IMG_BLK_SKP=65536  ; IMG_BLK_CNT=49152  ; TYPE_OF_IMG="-K (only kernel)"   ;;
+            B) IMG_BLK_SKP=114688 ; IMG_BLK_CNT=65536  ; TYPE_OF_IMG="-B (only boot)"     ;;
+            h|?) ota_usages && exit 1 ;;
+        esac
+    done
+
+    if [[ ${flag} -eq 0 ]] && [[ ${SOM_IMG_VER} != 0 ]]; then
+    	IMG_BLK_SKP=${BAK_BLK_SKP}
+    	IMG_BLK_CNT=${BAK_BLK_CNT}
+    fi
+    echo "IMG_BLK_SKP: $IMG_BLK_SKP, IMG_BLK_CNT: $IMG_BLK_CNT"
+
+    return $flag # max return 255
 }
 
 function ota_deploy_curr_package()
 {
     echo "Be about to deploy the OTA package..."
 
-    [ ${SOM_IMG_VER} = 0 ] && echo "[Error: No ota image file!]" && ota_usages && return 1
+    [[ ${SOM_IMG_VER} = 0 ]] && echo "[Error: No ota image file!]" && ota_usages && return 1
+    [[ ${IMG_BLK_SKP} -ne ${BAK_BLK_SKP} ]] && echo "[Error: fireware version incompatibility!]" && ota_usages && return 1
+    [[ ${IMG_BLK_CNT} -ne ${BAK_BLK_CNT} ]] && echo "[Error: fireware version incompatibility!]" && ota_usages && return 1
+
+    local img_realpath="$EXTRACT_DIR/$OTA_TMP_DIR/$SOM_IMG_NAM"
+    if [ -z "$EXTRACT_DIR" ] && [ -f "$SOM_IMG_NAM" ]; then
+        img_realpath="$SOM_IMG_NAM"
+    fi
+    [ ! -f "$img_realpath" ] && echo "[Err: No image file{$tmp_realpath}!]" && return 1
 
     echo "Read out current image and try comparing md5"
-    echo ${TEMI_PASSWD} | sudo -S dd if="$IMG_DEV_NAM" of="$TMPFILEPATH" skip=1 bs=512 count=${IMG_BLK_CNT} conv=sync
+    echo ${TEMI_PASSWD} | sudo -S dd if="$IMG_DEV_NAM" of="$TMPFILEPATH" skip=${IMG_BLK_SKP} bs=512 count=${IMG_BLK_CNT} conv=sync
     local md5val=`md5sum ${TMPFILEPATH} | cut -d ' ' -f1`
 
     if [ "${SOM_IMG_MD5}" != "${md5val}" ]; then
         echo "Backup old image to ${TMPFILEPATH}_bk"
         cp ${TMPFILEPATH} ${TMPFILEPATH}_bk
         echo "Start upgrading..."
-        local img_realpath="$EXTRACT_DIR/$OTA_TMP_DIR/$SOM_IMG_NAM"
-        if [ -z "$EXTRACT_DIR" ] && [ -f "$SOM_IMG_NAM" ]; then
-        	img_realpath="$SOM_IMG_NAM"
-        fi
-        [ ! -f "$img_realpath" ] && echo "[Err: No image file!]" && return 1
-        echo ${TEMI_PASSWD} | sudo -S dd if="$img_realpath" of="$IMG_DEV_NAM" seek=1 bs=512 count=${IMG_BLK_CNT} conv=sync
+        echo ${TEMI_PASSWD} | sudo -S dd if="$img_realpath" of="$IMG_DEV_NAM" seek=${IMG_BLK_SKP} bs=512 count=${IMG_BLK_CNT} conv=sync
         sync
         echo "Flashed, check upgraded file."
-        echo ${TEMI_PASSWD} | sudo -S dd if="$IMG_DEV_NAM" of="$TMPFILEPATH" skip=1 bs=512 count=${IMG_BLK_CNT} conv=sync
+        echo ${TEMI_PASSWD} | sudo -S dd if="$IMG_DEV_NAM" of="$TMPFILEPATH" skip=${IMG_BLK_SKP} bs=512 count=${IMG_BLK_CNT} conv=sync
         sync
         md5val=`md5sum ${TMPFILEPATH} | cut -d ' ' -f1`
         if [ "${SOM_IMG_MD5}" = "${md5val}" ]; then
@@ -138,47 +204,44 @@ function ota_create_new_package()
         return 2
     fi
 
-    echo ${TEMI_PASSWD} | sudo -S dd if="$IMG_DEV_NAM" of="$TMPFILEPATH" skip=1 bs=512 count=${IMG_BLK_CNT} conv=sync
+    echo ${TEMI_PASSWD} | sudo -S dd if="$IMG_DEV_NAM" of="$TMPFILEPATH" skip=${IMG_BLK_SKP} bs=512 count=${IMG_BLK_CNT} conv=sync
     local md5val=`md5sum ${TMPFILEPATH} | cut -d ' ' -f1`
-    cp ${TMPFILEPATH} ${OTA_TMP_DIR}/ota-${md5val}.img
-    sed -i "s/^SOM_IMG_NAM=.*$/SOM_IMG_NAM=ota-${md5val}.img/" ${EXEREALPATH}
+    local imgsiz=`du -b "$TMPFILEPATH" | awk '{print $1}'`
+    if [[ ${IMG_BLK_CNT} -ne $((imgsiz/512)) ]]; then
+        echo "[Error: read image size! should ${IMG_BLK_CNT} but $((imgsiz/512))"
+    else
+        cp ${TMPFILEPATH} ${OTA_TMP_DIR}/${md5val}.img
+    fi
+    sed -i "s/^BAK_BLK_CNT=.*$/BAK_BLK_CNT=${IMG_BLK_CNT}/"    ${EXEREALPATH}
+    sed -i "s/^BAK_BLK_SKP=.*$/BAK_BLK_SKP=${IMG_BLK_SKP}/"    ${EXEREALPATH}
+    sed -i "s/^SOM_IMG_NAM=.*$/SOM_IMG_NAM=${md5val}.img/"     ${EXEREALPATH}
     sed -i "s/^SOM_IMG_VER=.*$/SOM_IMG_VER=${1}/"              ${EXEREALPATH}
     sed -i "s/^SOM_IMG_MD5=.*$/SOM_IMG_MD5=${md5val}/"         ${EXEREALPATH}
+    sed -i "s/^TYPE_OF_IMG=.*$/TYPE_OF_IMG='${TYPE_OF_IMG}'/"  ${EXEREALPATH}
     sed -i "s/^KERNEAL_VER=.*$/KERNEAL_VER='$(uname -v)'/"     ${EXEREALPATH}
     cp ${EXEREALPATH} ${OTA_TMP_DIR}/${SCRIPT_NAME}
     ${MAKESELF_SH} makeself -s ${OTA_PACKAGE} -p ${OTA_TMP_DIR} -e ${OTA_TMP_DIR}/${SCRIPT_NAME}
     md5val=`md5sum ${OTA_PACKAGE} | cut -d ' ' -f1`
-    mv ${OTA_PACKAGE} rk3399_linux_ota-v${1}-${md5val}.run
+    package_dist_name=rk3399_linux_ota-v${1}-${md5val}.run
+    mv ${OTA_PACKAGE} ${package_dist_name}
     rm -rf $OTA_TMP_DIR
     
     echo "New OTA package create successfully!"
-    echo "The execable ota named [rk3399_linux_ota-v${1}-${md5val}.run]."
+    echo "The execable ota named [${package_dist_name}]."
 }
 
 function ota_main()
 {
-    local ota_ver
-    local flag
+    ota_cmdline_parser $@
 
-    while getopts ":dunv:h" opt > /dev/null; do
-        case "$opt" in
-            d) flag=0 ; break ;;
-            u) flag=1 ;;
-            n) flag=2 ;;
-            v) ota_ver="$OPTARG" ;;
-            h|?)
-                ota_usages
-                return 0
-                ;;
-        esac
-    done
+    local retv=$?
 
-    if [ -z ${flag} ] || [ ${flag} = 0 ]; then
+    if   [[ $retv -eq 0 ]] || [[ $retv -eq 1 ]]; then
         ota_deploy_curr_package
-    elif [ -n "$ota_ver" ]; then
-        case $flag in
-            1) ota_update_curr_package $ota_ver ;;
-            2) ota_create_new_package  $ota_ver ;;
+    elif [[ ${SOM_IMG_VER} != 0 ]]; then
+        case $retv in
+            2) ota_create_new_package  $SOM_IMG_VER ;;
+            3) ota_update_curr_package $SOM_IMG_VER ;;
         esac
     else
         echo "*** [Warning: Version number must be entered!] ***"
@@ -192,4 +255,5 @@ ota_main $@
 # Author        Date        Version    Abstract
 #------------------------------------------------------------------------------
 # Kevin Zhou   2019-09-23   v1.0       Initial version create
+# Kevin Zhou   2020-04-30   v1.1       New feture to backup some compoment
 ###############################################################################
