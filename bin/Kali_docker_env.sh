f9426289da77cb7f79a9c7a3f20682cc287d0006
Kevin Zhou <zzl-ko@outlook.com>
2021-02-08 18:36:18 +0800

    更新Kali的docker运行脚本

--- bin/Kali_docker_env.sh
+++ bin/Kali_docker_env.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 # refer: https://www.kali.org/news/official-kali-linux-docker-images/
 
-docker_container_name=`docker container ls | awk 'NR>1 && $NF == "kali" {print $NF}'`
+dcn="Kali"
+docker_container_name=`docker container ls | awk 'NR>1 && $NF == "'"$dcn"'" {print $NF}'`
 
-if [ "kali" = "$docker_container_name" ]; then
-	echo "docker exec kali ..."
-	docker exec -it kali /bin/bash
+if [ "$dcn" = "$docker_container_name" ]; then
+	echo "docker exec $dcn ..."
+	docker exec -it ${dcn} /bin/bash
 else
-	docker_container_name=`docker container ls -a | awk 'NR>1 && $NF == "kali" {print $NF}'`
+	docker_container_name=`docker container ls -a | awk 'NR>1 && $NF == "'"$dcn"'" {print $NF}'`
 
-	if [ "kali" = "$docker_container_name" ]; then
-		echo "docker start kali ..."
-		docker start kali
-		#docker attach kali
-		docker exec -it kali /bin/bash
+	if [ "$dcn" = "$docker_container_name" ]; then
+		echo "docker start $dcn ..."
+		docker start ${dcn}
+		#docker attach $dcn
+		docker exec -it $dcn /bin/bash
 	else
-		echo "docker run kali ..."
-		docker run --name kali -v /home/$USER:/home/$USER -it --workdir / kalilinux/kali-rolling /bin/bash
+		echo "docker run $dcn ..."
+		#docker 的四种网络模式(https://www.jianshu.com/p/22a7032bb7bd)
+		docker run --name ${dcn} -v /home/$USER:/home/$USER -it --net=host --workdir / kalilinux/kali-rolling /bin/bash
+
+		# 1. 更新国内镜像源
+		#中科大
+		#deb http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib
+		#deb-src http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib
+		#阿里云
+		#deb http://mirrors.aliyun.com/kali kali-rolling main non-free contrib
+		#deb-src http://mirrors.aliyun.com/kali kali-rolling main non-free contrib
+		#清华大学
+		#deb http://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free
+		#deb-src https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free
+		#浙大
+		#deb http://mirrors.zju.edu.cn/kali kali-rolling main contrib non-free
+		#deb-src http://mirrors.zju.edu.cn/kali kali-rolling main contrib non-free
+		#东软大学
+		#deb http://mirrors.neusoft.edu.cn/kali kali-rolling/main non-free contrib
+		#deb-src http://mirrors.neusoft.edu.cn/kali kali-rolling/main non-free contrib
+		#官方源
+		#deb http://http.kali.org/kali kali-rolling main non-free contrib
+		#deb-src http://http.kali.org/kali kali-rolling main non-free contrib
+
+		# 2. 安装相关软件包
+		#https://www.kali.org/news/official-kali-linux-docker-images/
+		#apt-get update && apt-get install metasploit-framework
+		#apt install realtek-rtl88xxau-dkms aircrack-ng net-tools pciutils
+		#apt install bash-completion
+
+		#ref: http://pkg.kali.org/pkg/realtek-rtl88xxau-dkms#
 	fi
 fi
